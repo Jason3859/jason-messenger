20e65e2766f7a2ce1952f4c7f7e5e804aafed3dd
Jason3859 <augustinej438@gmail.com>
2025-09-04 10:35:51 +0530

    migrated from supabase to railway postgres db

--- messenger-server/src/main/kotlin/dev/jason/project/ktor/messenger/data/ProjectModule.kt
+++ messenger-server/src/main/kotlin/dev/jason/project/ktor/messenger/data/ProjectModule.kt
@@ -5,18 +5,24 @@ import dev.jason.project.ktor.messenger.data.database.UsersDatabaseRepository
 import dev.jason.project.ktor.messenger.domain.DatabaseRepository
 import dev.jason.project.ktor.messenger.domain.UserRepository
 import org.koin.dsl.module
+import java.net.InetAddress
 import java.sql.Connection
 import java.sql.DriverManager
 
 val projectModule = module {
     single<Connection> {
         DriverManager.getConnection(
-            "jdbc:postgresql://db.wybspetuojavvbpqyewv.supabase.co:5432/postgres?user=postgres&password=${
-                System.getenv(
-                    "SUPABASE_PASSWORD"
-                )
-            }&sslmode=require"
-        )
+            "jdbc:postgresql://${System.getenv("PGHOST")}:${System.getenv("PGPORT")}/${System.getenv("PGDATABASE")}" +
+                    "?user=${System.getenv("PGUSER")}&password=${System.getenv("PGPASSWORD")}&sslmode=require&options=-c%20TimeZone=UTC"
+        ).also {
+            it.createStatement().use { stmt ->
+                stmt.execute("SET TIMEZONE TO 'Asia/Kolkata'")
+            }
+
+            InetAddress.getByName("db.wybspetuojavvbpqyewv.supabase.co").also {
+                println("Supabase resolves to: ${it.hostAddress}")
+            }
+        }
     }
 
     single<DatabaseRepository> {
